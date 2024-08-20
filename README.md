ba867bee0f0bca81abdd9704196be390bfb2613c
Timothy jan <jan.timothy@gmail.com>
2024-08-20 09:47:12 -0700

    Added TodoItem model and database context

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # TodoApi-ASP.NetCoreWebAPI
-TodoApi using the ASP.NET Core Web API.
+TodoApi using the ASP.NET Core Web API. Controller-based web API that uses a database.
+
+<br>
+
+Design
+<img href="design.jpg" alt="design-screenshot">
+
+<br>
+
+Swagger Documentation <a href="https://learn.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger?view=aspnetcore-8.0">link</a>
+
+
+<ul>
+  <li>A model is a set of classes that represent the data that the app manages. The model for this app is the TodoItem class.</li>
+  <li>The database context is the main class that coordinates Entity Framework functionality for a data model.</li>
+</ul>
--- TodoApi/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb8ec12db7e90ccb28dcde13c294f6a549c019c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25cd04ce532ff2bff4cc12671e61547e81fd23b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
